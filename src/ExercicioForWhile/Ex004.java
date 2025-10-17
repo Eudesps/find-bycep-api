20e940b2730381657885451148e7d41f2f43caf7
José Eudes <jeudes894@gmail.com>
2025-10-17 16:59:19 -0300

    exercicio de for e while 004

---
+++ src/ExercicioForWhile/Ex004.java
@@ -0,0 +1,40 @@
+package ExercicioForWhile;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ex004 {
+    public static void main(String[] args) {
+        String digitado = "";
+        int acaoKey = 0;
+
+        String comando = "";
+        ArrayList<String> convidados = new ArrayList<>();
+
+        Scanner leitor = new Scanner(System.in);
+
+        while (!comando.contentEquals("sair")){
+            System.out.println("Digite o nome do convidado (ou 'ver' para visualizar a lista, 'sair' para terminar): ");
+            digitado = leitor.nextLine();
+            if (digitado.equalsIgnoreCase("ver")) {
+                System.out.println(convidados);
+            } else if (digitado.equalsIgnoreCase("sair")) {
+                comando = digitado;
+            }else{
+                for(String nome: convidados){
+                    if (nome.equalsIgnoreCase(digitado)) {
+                        System.out.println("Nome já existente na lista");
+                        acaoKey = -1;
+                    }
+                }
+                if (acaoKey != -1) {
+                    convidados.add(digitado);
+                    System.out.println("Salvo com sucesso!!");
+                }else{
+                    acaoKey = 0;
+                }
+            }
+        }
+        leitor.close();
+    }
+}
