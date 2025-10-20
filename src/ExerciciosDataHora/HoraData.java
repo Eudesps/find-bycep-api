3e0210b2fc2217c9e4b0e38320d51a42bed07d98
José Eudes <jeudes894@gmail.com>
2025-10-20 11:29:55 -0300

    formatando a data e hora atual

--- src/ExerciciosDataHora/HoraData.java
+++ src/ExerciciosDataHora/HoraData.java
@@ -2,12 +2,17 @@ package ExerciciosDataHora;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class HoraData {
     public static void main(String[] args) {
         LocalDate dataAtual = LocalDate.now();
         LocalTime horaAtual = LocalTime.now();
 
-        System.out.println("Data: " + dataAtual + " Hora: " + horaAtual);
+        DateTimeFormatter formatoData = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter formatoHora = DateTimeFormatter.ofPattern("hh:mm");
+
+
+        System.out.println("Data: " + dataAtual.format(formatoData) + " Hora: " + horaAtual.format(formatoHora));
     }
 }
